2e6af572f2d75ef2a992163357109f06669e5f28
DoCi Mk <80265901+DoCi-Mk@users.noreply.github.com>
2021-08-26 22:50:53 +0430

    say update

--- commands/say.js
+++ commands/say.js
@@ -5,12 +5,12 @@ exports.run = async (client, message , args) => {
     if(message.content.includes('@everyone'))return message.channel.send(
         new Discord.MessageEmbed()
         .setColor('#ff0a26')
-        .setDescription('**این کلمه را نمیتوان استفاده کرد <a:850335306657955840:856794054540394526> **')
+        .setDescription('**Dont Say This :x: **')
     )
     if(message.content.includes('@here'))return message.channel.send( 
     new Discord.MessageEmbed()
     .setColor('#ff0a26')
-    .setDescription('**این کلمه را نمیتوان استفاده کرد <a:850335306657955840:856794054540394526> **')
+    .setDescription('**Dont Say This :x: **')
     )
     if (message.content.includes("discord.gg/"))
     return message.reply(`:x: Yo don't try to post server link using me !!!`)
